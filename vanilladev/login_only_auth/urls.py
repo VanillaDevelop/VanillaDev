bab19be7fba1aa35fa1a3257ceb432a337cc632a
Vanilla <vanilla@nya.gg>
2022-02-13 20:29:34 +0100

    Replace login view with django auth login view, properly authenticates user

--- vanilladev/login_only_auth/urls.py
+++ vanilladev/login_only_auth/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path(
+        'login/',auth_views.LoginView.as_view(template_name='login_only_auth/login.html'),
+    ),
 ]
