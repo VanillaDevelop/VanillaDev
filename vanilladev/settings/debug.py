c1ffb08b749894604731e9aabd35cf239e67a188
Vanilla <vanilla@nya.gg>
2022-03-07 16:06:05 +0100

    Create separate settings file for debug and production

--- vanilladev/settings/debug.py
+++ vanilladev/settings/debug.py
@@ -19,18 +19,16 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@!5w2iap&(!%==p(&apob=n4v!rpp@zw3%^k%l-^&6dr80jxju'
+SECRET_KEY = os.getenv("SECRETKEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -79,7 +77,6 @@ WSGI_APPLICATION = 'vanilladev.wsgi.application'
 
 
 # Database
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -149,6 +146,5 @@ CKEDITOR_CONFIGS = {
 }
 
 LOGIN_URL = "/user/login"
-
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
