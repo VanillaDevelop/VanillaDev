18bd7b3822774b668272fa2eb939812a83ea1d4e
Vanilla <vanilla@nya.gg>
2022-04-03 18:09:57 +0200

    Add edit and delete for media

--- vanilladev/media/urls.py
+++ vanilladev/media/urls.py
@@ -6,5 +6,7 @@ app_name='media'
 urlpatterns = [
     path('add/', views.add, name='add'),
     path('overview/', views.overview, name='overview'),
+    path('overview/<int:pageno>/', views.overview, name='overview'),
     path('delete/<int:id>/', views.delete, name='delete'),
+    path('edit/<int:id>/', views.edit, name='edit'),
 ]
--- vanilladev/media/views.py
+++ vanilladev/media/views.py
@@ -1,6 +1,6 @@
 import math
 from vanilladev.helpers import render_mainpage
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ImageForm
 from .models import Image
@@ -52,3 +52,23 @@ def delete(request, id):
 
     #redirect the user to the media overview
     return redirect('media:overview')
+
+#edit image - requires login
+@login_required()
+def edit(request, id):
+    #POST - save image
+    if request.method == "POST":
+        image = get_object_or_404(Image, id=id)
+        form = ImageForm(request.POST, request.FILES, instance=image)
+        #if form is valid, redirect to the corresponding post
+        if form.is_valid():
+            form.save()
+            return redirect('media:overview')
+        else: 
+            #serve the sent form again
+            return render_mainpage(request, 'media/change.html', {"form":form})
+
+    #GET - show edit form for blog post
+    image = get_object_or_404(Image, id=id)
+    form = ImageForm(instance=image)
+    return render_mainpage(request, 'media/change.html', {"form":form})
