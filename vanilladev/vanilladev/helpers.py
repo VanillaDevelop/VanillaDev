8ef632d9c1958ffcbf695a231abf95e25b9fc7f1
Vanilla <vanilla@nya.gg>
2022-04-07 18:21:17 +0200

    Navbar changes: Logged in users can see all projects and articles in the dropdown. Draft icon added to unpublished projects and articles in the navbar. Users that are not logged in see a disabled navlink if there are no published projects or articles.

--- vanilladev/vanilladev/helpers.py
+++ vanilladev/vanilladev/helpers.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from projects.models import Project
+from articles.models import Article
 
  #helper function for adding information the template needs to the request
 def render_mainpage(request, page, vars = {}):
-    #add all public projects for the navbar
-    projects = Project.objects.filter(is_published=True).values('id', 'title')
+    #add navbar entries (depending on logged in or not)
+    if(request.user.is_authenticated):
+        projects = Project.objects.all().values('id', 'title', 'is_published')
+        articles = Article.objects.all().values('id', 'title', 'is_published')
+    else:
+        projects = Project.objects.filter(is_published=True).values('id', 'title', 'is_published')
+        articles = Article.objects.filter(is_published=True).values('id', 'title', 'is_published')
+
     vars["navbar_projects"] = projects
+    vars["navbar_articles"] = articles
     return render(request, page, vars)
