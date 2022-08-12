8eb754dcdb6f32761e09a643be8341fb169f3c36
Vanilla <vanilla@nya.gg>
2022-08-12 20:37:48 +0200

    path fiesta

--- vanilladev/vanilladev/settings_debug.py
+++ vanilladev/vanilladev/settings_debug.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = os.getenv("SECRETKEY")
 DEBUG = True
 ALLOWED_HOSTS = ['*']
